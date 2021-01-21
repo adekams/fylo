dea8a096a66017c2e4ae61b4033f2ca5976711db
AdenikeSalau <kifademi@gmail.com>
2021-01-21 12:57:13 +0100

    dark-theme styles changed

--- js/script.js
+++ js/script.js
@@ -1,21 +1,13 @@
+
 let moon = document.querySelector('#switch i')
-    moon.addEventListener("click", changeMode)
+moon.addEventListener("click", changeMode);
 
-    function changeMode () {
-        document.body.classList.toggle('dark-theme')
-        
-        let sections = Array.from(document.querySelectorAll("section"))
-        sections.map(section => {
-            section.classList.toggle('dark-theme')
-        })
+function changeMode () {
+    document.body.classList.toggle('dark-theme');
+    
+    let sections = Array.from(document.querySelectorAll("section"))
+    sections.map((section) => {
+        section.classList.toggle('dark-theme');
 
-        let sectionsEven = Array.from(document.querySelectorAll("main section:nth-child(even)"))
-
-        sectionsEven.map(section =>{
-            // console.log("even section")
-            section.classList.toggle("dark-theme")
-        })
-
-        // sections.forEach(section =>
-            
-    }
+    })
+}
